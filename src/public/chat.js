5b8e2038aac139be528da7a4e1822c133560ab63
Gederson <gedersongustavogbot@ufps.edu.co>
2023-01-02 11:47:47 -0500

    Chat V1.2

--- src/public/chat.js
+++ src/public/chat.js
@@ -24,7 +24,7 @@ socket.on("chat:message", function (data) {
   output.innerHTML += `<p class="animate__animated animate__fadeIn">
     <strong>${data.username}: </strong> ${data.message}
     </p>`;
-  message = "";
+  message.value = "";
 });
 
 socket.on("chat:typing", function (data) {
