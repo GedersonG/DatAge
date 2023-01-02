56ef11d89245ee1780c0fa907d58f08a6e5c4a58
Gederson <gedersongustavogbot@ufps.edu.co>
2023-01-01 21:42:57 -0500

    Code production

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import app from "./app";
 import { Server as WebSocketServer } from "socket.io";
 import http from "http";
 import sockets from "./sockets";
+import { PORT } from "./config";
 import { connectDB } from "./db";
 
 connectDB();
 
 const server = http.createServer(app);
-const httpServer = server.listen(3000);
-console.log("Server is listening on port 3000");
+const httpServer = server.listen(PORT);
+console.log("Server is listening on port ", PORT);
 
 const io = new WebSocketServer(httpServer);
 sockets(io);
