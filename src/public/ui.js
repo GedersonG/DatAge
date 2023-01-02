50d969b16cb7a4b681111b60d17761b8d6ea48d6
Gederson <gedersongustavogbot@ufps.edu.co>
2023-01-01 21:27:44 -0500

    Boostrap and animate CSS

--- src/public/ui.js
+++ src/public/ui.js
@@ -1,19 +1,21 @@
 import { saveNote, deleteNote, getNoteById, updateNote } from "./socket.js";
 
 const noteList = document.querySelector("#notes");
-const title = document.querySelector("#title")
-const description = document.querySelector("#description")
+const title = document.querySelector("#title");
+const description = document.querySelector("#description");
 
 let saveId = "";
 
 const noteUI = (note) => {
   const div = document.createElement("div");
   div.innerHTML = `
-  <div>
-    <h1>${note.title}</h1>
+  <div class="card card-title rounded-0 animate__animated animate__fadeInUp">
+    <h3>${note.title}</h3>
+    <div class="d card-body justify-content-between mb-2">
+        <button class="btn btn-danger btn-sm delete" data-id="${note._id}">Delete</button>
+        <button class="btn btn-secondary btn-sm update" data-id="${note._id}">Update</button>
+    </div>
     <p>${note.description}</p>
-    <button class="delete" data-id="${note._id}">Delete</button>
-    <button class="update" data-id="${note._id}">Update</button>
   </div>
 `;
 
@@ -22,7 +24,7 @@ const noteUI = (note) => {
 
   btnDelete.addEventListener("click", (e) => deleteNote(btnDelete.dataset.id));
   btnUpdate.addEventListener("click", (e) => getNoteById(btnUpdate.dataset.id));
-  
+
   return div;
 };
 
@@ -31,25 +33,24 @@ export const renderNotes = (notes) => {
   notes.forEach((note) => noteList.append(noteUI(note)));
 };
 
-export const fillForm = note => {
-  title.value = note.title
-  description.value = note.description
+export const fillForm = (note) => {
+  title.value = note.title;
+  description.value = note.description;
   saveId = note._id;
-}
+};
 
 export const onHandleSubmit = (e) => {
   e.preventDefault();
 
-  if(saveId) {
+  if (saveId) {
     updateNote(saveId, title.value, description.value);
-  } else{
+  } else {
     saveNote(title.value, description.value);
   }
 
-  saveId =""
-  title.value=""
-  description.value=""
-
+  saveId = "";
+  title.value = "";
+  description.value = "";
 };
 
 export const appendNote = (note) => {
